d2911fe2736ac4181f7e5004f6ea8db646fa76e2
myang42 <32368818+myang42@users.noreply.github.com>
2017-12-30 19:49:27 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
 		$fd = fopen($elem, "r");
 		while($clothes[$e][$i] = fgetcsv($fd, 0, ","))
 		{
-			if (preg_match('/^[A-aZ-z|\s]+/',$clothes[$e][$i][2]) == TRUE)
+			if (preg_match('/^[A-aZ-z|\s|-]+/',$clothes[$e][$i][2]) == TRUE)
 			{
-				if($clothes[$e][$i][3] != NULL)
+				if($clothes[$e][$i][2] != NULL)
 					$i++;
 			}
 		}
@@ -42,12 +42,14 @@
 
 	function ttomult($tiers, $i)
 	{
-		if ($tiers[$i] == 3)
-			return(2.0);
+		if ($tiers[$i] == 4)
+			return(1.66);
+		else if ($tiers[$i] == 3)
+			return(1.33);
 		else if ($tiers[$i] == 2)
-			return(1.5);
-		else if ($tiers[$i] == 1)
 			return(1.0);
+		else if ($tiers[$i] == 1)
+			return(.66);
 		else
 			return(0);
 	}
@@ -59,22 +61,28 @@
 		{
 			for($i = 3; $i < 13; $i++)
 			{
+				// echo $tiers[$i - 3]."\t";
+				// echo $elem[$i]."\t";
 				$tl = ttomult($tiers, $i - 3);
-				$res[$j] = $res[$j] + atopts(strtoupper($elem[$i])) * $tl;
+				$res[$j] = $res[$j] + (atopts(strtoupper($elem[$i])) * $tl);
+				if ($tags != NULL){
 				if (strtoupper($elem[14]) == $tags || strtoupper($elem[15]) == $tags)
-					$res[$j] += 3;
+					$res[$j] += 3;}
+				// echo "= ".$res[$j]."\t";
+				// echo $elem[2]."\n";
 			}
 			$j++;
+			// echo "\n";
 		}
-		print_r($res);
+		// print_r($res);
 		$r = max($res);
 		$ar = array_keys($res, $r);
 		return($ar[0]);
 	}
 
-	$tl = array(0,1,0,2,0,0,3,2,0,3);
+	$tl = array(0,4,0,2,0,3,0,4,4,0);
 	foreach($clothes as $elem)
-		$v[] = calc_val($elem, $tl, "Denim");
+		$v[] = calc_val($elem, $tl, "");
 	$h = 0;
 	foreach($v as $value)
 	{
